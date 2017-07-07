68bdaefa872442684d97ac5a9fa456c61028b66f
Filipe Brandao <fdabrandao@gmail.com>
2017-07-07 01:29:27 +0100

    Include the tests in the packages

--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ cd $BASEPATH
 
 source venv/bin/activate
 pip install -r requirements.txt
-nosetests tests --with-coverage --cover-erase --cover-html --cover-html-dir=$BASEPATH/cover
+pip install . --upgrade
+nosetests amplpy.tests examples --with-coverage --cover-erase --cover-html --cover-html-dir=$BASEPATH/cover
