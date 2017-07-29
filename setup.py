2657e9ba1dcf5f365114210a24df562f97955827
Filipe Brandao <fdabrandao@gmail.com>
2017-07-29 16:35:31 +0100

    Minimal setup of cibuildwheel

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ libdir = 'lib64' if x64 else 'lib32'
 
 setup(
     name='amplpy',
-    version='0.1.0',
+    version='0.1.1a2',
     description='Python API for AMPL',
     long_description=__doc__,
     license='BSD-3',
