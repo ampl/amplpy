acfc7cecccce15377720f75ef942ac4db0e4b90a
Filipe Brandao <fdabrandao@gmail.com>
2017-08-03 22:04:35 +0100

    Ignore unnecessary scripts

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BASEDIR=`dirname $0`
-BASEPATH=`realpath $BASEDIR`
-cd $BASEPATH
-
-source venv/bin/activate
-pip install -r requirements.txt
-pip install . --upgrade
-nosetests amplpy.tests examples --with-coverage --cover-erase --cover-html --cover-html-dir=$BASEPATH/cover --cover-package amplpy
