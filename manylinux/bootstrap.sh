9afa97df7c1b6205cd2b492e37e93b9832f5c202
Filipe Brandao <fdabrandao@gmail.com>
2018-10-25 00:38:52 +0100

    Update manylinux scripts

--- manylinux/bootstrap.sh
+++ manylinux/bootstrap.sh
@@ -4,15 +4,15 @@ if [ "$#" -eq 0 ]; then
   	echo "Usage: $0 [32|64]"
 else
     cd /tmp/
-    wget http://www.cmake.org/files/v3.0/cmake-3.0.0.tar.gz --no-check-certificate
-    tar xzvf cmake-3.0.0.tar.gz
-    cd cmake-3.0.0
+    curl -O https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
+    tar xzvf cmake-3.12.3.tar.gz
+    cd cmake-3.12.3
     ./bootstrap
     gmake
     gmake install
 
     cd /tmp/
-    wget http://ampl.com/demo/ampl.linux$1.tgz
+    curl -O https://ampl.com/demo/ampl.linux$1.tgz
     tar xzvf ampl.linux$1.tgz
     mv ampl.linux$1 /opt/
 
