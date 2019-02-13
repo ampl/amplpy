89a232538c2ce7bc3ed406e6b9587cebbff2849e
Filipe Brandao <fdabrandao@gmail.com>
2019-02-13 21:30:01 +0000

    Add workaround for conda python
    Python versions that come with conda for Windows
    do not load .dlls in the same way as standard
    python versions.

--- amplpy/amplpython/__init__.py
+++ amplpy/amplpython/__init__.py
@@ -2,14 +2,22 @@
 from __future__ import absolute_import
 
 import os
+import ctypes
 import platform
 
 if platform.system() == 'Windows':
     lib32 = os.path.join(os.path.dirname(__file__), 'lib32')
     lib64 = os.path.join(os.path.dirname(__file__), 'lib64')
-    os.environ['PATH'] += os.pathsep + lib32
-    os.environ['PATH'] += os.pathsep + lib64
-
+    from glob import glob
+    from ctypes import sizeof
+    try:
+        if ctypes.sizeof(ctypes.c_voidp) == 4:
+            dllfile = glob(lib32 + '/*.dll')[0]
+        else:
+            dllfile = glob(lib64 + '/*.dll')[0]
+        ctypes.CDLL(dllfile)
+    except:
+        pass
 
 from .amplpython import *
 from .amplpython import _READTABLE, _WRITETABLE
