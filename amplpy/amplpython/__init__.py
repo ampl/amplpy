e71256921c89899d819ded09d61c0789da927766
Filipe Brandao <fdabrandao@gmail.com>
2019-02-13 22:53:54 +0000

    Remove superfluous import

--- amplpy/amplpython/__init__.py
+++ amplpy/amplpython/__init__.py
@@ -9,7 +9,6 @@ if platform.system() == 'Windows':
     lib32 = os.path.join(os.path.dirname(__file__), 'lib32')
     lib64 = os.path.join(os.path.dirname(__file__), 'lib64')
     from glob import glob
-    from ctypes import sizeof
     try:
         if ctypes.sizeof(ctypes.c_voidp) == 4:
             dllfile = glob(lib32 + '/*.dll')[0]
