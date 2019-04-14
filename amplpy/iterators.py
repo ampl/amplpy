b044bc3ab489c80cd54efdc2d62c311a488c7ed6
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 22:40:09 -0700

    Fixed PYTHON_F401 issues in amplpy/iterators.py

--- amplpy/iterators.py
+++ amplpy/iterators.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, absolute_import, division
-from builtins import map, range, object, zip, sorted
+from builtins import object
 from past.builtins import basestring
 
 from .utils import Utils, Tuple
