f55254434e9040cfe877729dc0d7a8621c5c7d73
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 22:41:17 -0700

    Fixed PYTHON_F401 issues in amplpy/__init__.py

--- amplpy/__init__.py
+++ amplpy/__init__.py
@@ -1,18 +1,3 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from .outputhandler import OutputHandler, Kind
-from .errorhandler import ErrorHandler
-from .exceptions import AMPLException
-from .iterators import EntityMap
-from .runnable import Runnable
-from .objective import Objective
-from .variable import Variable
-from .constraint import Constraint
-from .set import Set
-from .parameter import Parameter
-from .entity import Entity
-from .dataframe import DataFrame
-from .utils import multidict, register_magics
-from .environment import Environment
-from .ampl import AMPL
 __version__ = '0.6.5'
