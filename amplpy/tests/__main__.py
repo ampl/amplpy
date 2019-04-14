bd04ce08239d0be580ed99234255cf61c0c00566
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 22:40:17 -0700

    Fixed PYTHON_F401 issues in amplpy/tests/__main__.py

--- amplpy/tests/__main__.py
+++ amplpy/tests/__main__.py
@@ -3,12 +3,6 @@
 from __future__ import absolute_import
 
 import unittest
-from .test_ampl import TestAMPL
-from .test_entities import TestEntities
-from .test_iterators import TestIterators
-from .test_dataframe import TestDataFrame
-from .test_environment import TestEnvironment
-from .test_properties import TestProperties
 
 
 if __name__ == '__main__':
