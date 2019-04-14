9cfd0a970f133f9b8dce191e28413b248b35e1ff
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 22:40:38 -0700

    Fixed PYTHON_F401 issues in amplpy/outputhandler.py

--- amplpy/outputhandler.py
+++ amplpy/outputhandler.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, absolute_import, division
-from builtins import map, range, object, zip, sorted
-from past.builtins import basestring
+from builtins import object
 
 from . import amplpython
 
