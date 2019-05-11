94bd847a634d3f69ce5ce4842efa434596e5092b
Filipe Brandao <fdabrandao@gmail.com>
2019-05-11 20:14:25 +0100

    Update documentation

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 (amplpy-0.6.5)',
+    VERSION: '2.0 (amplpy-0.6.6)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
