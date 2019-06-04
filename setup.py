a979e41820912c5f8abcd8c23e0a596ea17b4499
Filipe Brandao <fdabrandao@gmail.com>
2019-06-04 17:44:55 +0100

    Prepare test release v0.6.7b1

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ libdir = 'lib64' if x64 else 'lib32'
 
 setup(
     name='amplpy',
-    version='0.6.7b0',
+    version='0.6.7b1',
     description='Python API for AMPL',
     long_description=__doc__,
     license='BSD-3',
