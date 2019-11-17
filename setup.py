1b86e3123c47e22a06d9f7c50ebeae1957e412c5
Filipe Brandao <fdabrandao@gmail.com>
2019-11-17 00:05:34 +0000

    Prepare release v0.6.9

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ cppinterface = os.path.join('amplpy', 'amplpython', 'cppinterface')
 
 setup(
     name='amplpy',
-    version='0.6.9b1',
+    version='0.6.9',
     description='Python API for AMPL',
     long_description=__doc__,
     license='BSD-3',
