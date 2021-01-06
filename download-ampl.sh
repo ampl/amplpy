cb6a3d68be91c99d66e8d67d1c60ad88a0c3f7e8
Filipe Brandão <fdabrandao@gmail.com>
2021-01-06 20:35:53 +0000

    Add download-ampl.sh

---
+++ download-ampl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd `dirname $0`
+BASEDIR=`pwd`
+if [ "$#" -ne 1 ]; then
+  	echo "Usage: $0 <URL>"
+    exit 1
+fi
+set -ex
+
+URL=$1
+PACKAGE=`basename $URL`
+curl -k -O $URL
+if [[ $PACKAGE == *.zip ]]; then
+    unzip $PACKAGE
+else
+    tar xzvf $PACKAGE
+fi
+rm $PACKAGE
+mv ampl.* ampl
+cd ampl
+pwd
