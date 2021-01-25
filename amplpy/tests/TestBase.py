48394c55599968c456f1f58c0fcdf58e1750f293
Filipe Brandão <fdabrandao@gmail.com>
2021-01-25 17:46:31 +0000

    Add workaround for tests on MSYS2 and MINGW

--- amplpy/tests/TestBase.py
+++ amplpy/tests/TestBase.py
@@ -10,19 +10,33 @@ import shutil
 import os
 
 
+# For MSYS2, MINGW, etc., run with:
+# $ REAL_ROOT=`cygpath -w /` python -m amplpy.tests
+REAL_ROOT = os.environ.get('REAL_ROOT', None)
+
+
 class TestBase(unittest.TestCase):
     def setUp(self):
         self.ampl = amplpy.AMPL()
         self.dirpath = tempfile.mkdtemp()
 
+    def _tmpfile(self, filename):
+        return os.path.join(self.dirpath, filename)
+
+    def _real_filename(self, filename):
+        # Workaround for MSYS2, MINGW paths
+        if REAL_ROOT is not None and filename.startswith('/'):
+            filename = filename.replace('/', REAL_ROOT, 1)
+        return filename
+
     def str2file(self, filename, content):
-        fullpath = self.tmpfile(filename)
+        fullpath = self._tmpfile(filename)
         with open(fullpath, 'w') as f:
             print(content, file=f)
-        return fullpath
+        return self._real_filename(fullpath)
 
     def tmpfile(self, filename):
-        return os.path.join(self.dirpath, filename)
+        return self._real_filename(self._tmpfile(filename))
 
     def tearDown(self):
         self.ampl.close()
