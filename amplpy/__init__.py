e5925970f0376f598e8f0f1282b081730721ddda
Filipe Brandão <fdabrandao@gmail.com>
2021-12-15 12:39:39 +0000

    Provide snake_case version of all methods

--- amplpy/__init__.py
+++ amplpy/__init__.py
@@ -16,3 +16,16 @@ from .utils import multidict, register_magics
 from .environment import Environment
 from .ampl import AMPL
 __version__ = '0.8.0b0'
+
+import inflection
+for cls in [
+        OutputHandler, ErrorHandler,
+        AMPLException, EntityMap, Runnable,
+        Entity, Objective, Variable, Constraint, Set, Parameter,
+        DataFrame, Environment, AMPL]:
+    methods = list(dir(cls))
+    for method in methods:
+        snake_method = inflection.underscore(method)
+        if snake_method != method and snake_method not in methods:
+            # print(cls, snake_method, method)
+            setattr(cls, snake_method, getattr(cls, method))
