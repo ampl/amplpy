a9cb83e2bffe86368e3668abfc8252e9fcdf6703
Filipe Brandão <fdabrandao@gmail.com>
2023-02-23 12:26:46 +0000

    Delete manylinux scripts

--- manylinux/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-
-docker run -v `pwd`/..:/shared -it --rm manylinux32 /shared/manylinux/script_test.sh 32 || exit 1
-docker run -v `pwd`/..:/shared -it --rm manylinux64 /shared/manylinux/script_test.sh 64 || exit 1
-
