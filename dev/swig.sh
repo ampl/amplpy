afb5fc3968404d9395cf71fec05e36e166ff06fc
Filipe Brandão <fdabrandao@gmail.com>
2023-03-14 11:17:24 +0000

    Move swig to dev/

--- dev/swig.sh
+++ dev/swig.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd "`dirname "$0"`"
-cd ..
 
 cd swig
-swig -python -c++ -builtin -o ../amplpy/amplpython/cppinterface/amplpythonPYTHON_wrap.cxx \
+swig -python -c++ -builtin -o ../../amplpy/amplpython/cppinterface/amplpythonPYTHON_wrap.cxx \
     -I../amplpy/amplpython/cppinterface/include \
     python/amplpython.i
