1e78ae69d5fdcacd9e5d7f8b532a5cc866bd8b53
Jurgen Lentz <jurgenlentz26@gmail.com>
2025-10-13 14:31:03 +0200

    start with typehints

---
+++ amplpy/__init__.pyi
@@ -0,0 +1,23 @@
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generator,
+    List,
+    Literal,
+    Iterable,
+    Mapping,
+    Optional,
+    Sequence,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+    overload
+)
+
+
+class AMPL:
+    def __init__(self, environment: Optional[Environment] = None) -> None: ...
+
+
