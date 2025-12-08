95ac2ea4ea0370bf726c929b1964187a54074156
Jürgen Lentz <jurgenlentz26@gmail.com>
2025-12-08 17:55:48 +0100

    fix classifier of amplpy v0.16.0

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
-        "Programming Language :: Python :: 3.14t",
+        "Programming Language :: Python :: Free Threading :: 2 - Beta",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     packages=["amplpy"],
